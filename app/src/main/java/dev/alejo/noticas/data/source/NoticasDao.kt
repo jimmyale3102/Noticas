b6c5171cf468ba840fa8bf23b8e32aea1ffc86c5
Jimmyale3102 <jimmyale3102@gmail.com>
2025-04-08 22:25:52 +0700

    feat: Add NoticasDao for database operations

---
+++ app/src/main/java/dev/alejo/noticas/data/source/NoticasDao.kt
@@ -0,0 +1,26 @@
+package dev.alejo.noticas.data.source
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import dev.alejo.noticas.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoticasDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertNote(note: Note)
+
+    @Query("SELECT * FROM note WHERE id = :id")
+    fun getNoteById(id: Int): Note?
+
+    @Query("SELECT * FROM note")
+    fun getAllNotes(): Flow<List<Note>>
+
+    @Delete
+    fun deleteNote(note: Note)
+
+}
