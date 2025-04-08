6d1897c2001857b149b7507b070dbdf6ba272dac
Jimmyale3102 <jimmyale3102@gmail.com>
2025-04-08 22:25:34 +0700

    feat: Create Note data class

---
+++ app/src/main/java/dev/alejo/noticas/domain/model/Note.kt
@@ -0,0 +1,37 @@
+package dev.alejo.noticas.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import dev.alejo.noticas.ui.theme.coralPink
+import dev.alejo.noticas.ui.theme.lavender
+import dev.alejo.noticas.ui.theme.lightGreen
+import dev.alejo.noticas.ui.theme.lightLilac
+import dev.alejo.noticas.ui.theme.mintGreen
+import dev.alejo.noticas.ui.theme.paleTeal
+import dev.alejo.noticas.ui.theme.pinkViolet
+import dev.alejo.noticas.ui.theme.skyBlue
+import dev.alejo.noticas.ui.theme.softOrange
+import dev.alejo.noticas.ui.theme.warmBeige
+
+@Entity
+data class Note(
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int? = null
+) {
+    companion object {
+        val noteColors = listOf(
+            pinkViolet,
+            coralPink,
+            lightGreen,
+            softOrange,
+            mintGreen,
+            lavender,
+            paleTeal,
+            lightLilac,
+            skyBlue, warmBeige
+        )
+    }
+}
