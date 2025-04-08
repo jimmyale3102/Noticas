91e31232d52cf3bd16f0d349163a8a0e3a012ede
Jimmyale3102 <jimmyale3102@gmail.com>
2025-04-08 17:20:11 +0700

    feat: Add SplashScreen API integration

--- app/src/main/java/dev/alejo/noticas/MainActivity.kt
+++ app/src/main/java/dev/alejo/noticas/MainActivity.kt
@@ -5,18 +5,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import dev.alejo.noticas.ui.theme.NoticasTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             NoticasTheme {
