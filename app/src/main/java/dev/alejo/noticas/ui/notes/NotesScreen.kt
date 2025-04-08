bc296bd4a1360f0e6683302ef4afb6b82cf76ef5
Jimmyale3102 <jimmyale3102@gmail.com>
2025-04-08 22:25:41 +0700

    feat: Add Note model and NotesScreen

---
+++ app/src/main/java/dev/alejo/noticas/ui/notes/NotesScreen.kt
@@ -0,0 +1,14 @@
+package dev.alejo.noticas.ui.notes
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NotesScreen() {
+    Column(Modifier.fillMaxSize()) {
+        Text("Heyyyy")
+    }
+}
