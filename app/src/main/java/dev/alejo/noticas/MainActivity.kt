3ff8820a87b8a4457af6f3b8bfdfa3f08f0868f4
Jimmyale3102 <jimmyale3102@gmail.com>
2025-04-09 17:06:04 +0700

    refactor: Apply inner padding to NavigationWrapper using modifier

--- app/src/main/java/dev/alejo/noticas/MainActivity.kt
+++ app/src/main/java/dev/alejo/noticas/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -23,7 +24,10 @@ class MainActivity : ComponentActivity() {
             NoticasTheme {
                 val navController = rememberNavController()
                 Scaffold(Modifier.fillMaxSize()) { innerPadding ->
-                    NavigationWrapper(navController = navController, innerPadding = innerPadding)
+                    NavigationWrapper(
+                        navController = navController,
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
