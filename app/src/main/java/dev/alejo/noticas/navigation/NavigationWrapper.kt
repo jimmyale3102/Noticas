5cdd430e58286f73f96b11c6a0789d7af04004cf
Jimmyale3102 <jimmyale3102@gmail.com>
2025-04-09 17:06:12 +0700

    refactor: Implement state handling in NavigationWrapper

--- app/src/main/java/dev/alejo/noticas/navigation/NavigationWrapper.kt
+++ app/src/main/java/dev/alejo/noticas/navigation/NavigationWrapper.kt
@@ -2,26 +2,30 @@ package dev.alejo.noticas.navigation
 
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionLayout
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import dev.alejo.noticas.ui.AddEditNoteScreen
+import dev.alejo.noticas.ui.add_edit_note.AddEditNoteEvent
+import dev.alejo.noticas.ui.add_edit_note.AddEditNoteScreen
+import dev.alejo.noticas.ui.add_edit_note.AddEditNoteViewModel
 import dev.alejo.noticas.ui.notes.NotesScreen
 import dev.alejo.noticas.ui.notes.NotesViewModel
 
 @OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
-fun NavigationWrapper(navController: NavHostController, innerPadding: PaddingValues) {
+fun NavigationWrapper(navController: NavHostController, modifier: Modifier) {
     SharedTransitionLayout {
         NavHost(navController = navController, startDestination = Screens.Notes) {
             composable<Screens.Notes> {
                 val viewModel = hiltViewModel<NotesViewModel>()
-                val state = viewModel.state.value
+                val state by viewModel.state.collectAsStateWithLifecycle()
                 NotesScreen(
-                    superInnerPadding = innerPadding,
+                    modifier = modifier,
                     state = state,
                     animatedVisibilityScope = this
                 ) {
@@ -29,7 +33,16 @@ fun NavigationWrapper(navController: NavHostController, innerPadding: PaddingVal
                 }
             }
             composable<Screens.AddEditNote> {
-                AddEditNoteScreen(animatedVisibilityScope = this)
+                val viewModel = hiltViewModel<AddEditNoteViewModel>()
+                val state by viewModel.state.collectAsStateWithLifecycle()
+                AddEditNoteScreen(
+                    modifier = modifier,
+                    animatedVisibilityScope = this,
+                    state = state,
+                    onColorSelected = { color ->
+                        viewModel.onEvent(AddEditNoteEvent.ChangeColor(color))
+                    }
+                )
             }
         }
     }
