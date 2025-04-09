22140214b2e015b55c24bd4ab5edebb3fca1c66a
Jimmyale3102 <jimmyale3102@gmail.com>
2025-04-09 15:39:55 +0700

    feat: Implement Navigation with SharedTransitionLayout

--- app/src/main/java/dev/alejo/noticas/navigation/NavigationWrapper.kt
+++ app/src/main/java/dev/alejo/noticas/navigation/NavigationWrapper.kt
@@ -1,20 +1,36 @@
 package dev.alejo.noticas.navigation
 
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionLayout
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import dev.alejo.noticas.ui.AddEditNoteScreen
 import dev.alejo.noticas.ui.notes.NotesScreen
+import dev.alejo.noticas.ui.notes.NotesViewModel
 
+@OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
 fun NavigationWrapper(navController: NavHostController, innerPadding: PaddingValues) {
-    NavHost(navController = navController, startDestination = Screens.Notes) {
-        composable<Screens.Notes> {
-            NotesScreen(superInnerPadding = innerPadding)
-        }
-        composable<Screens.AddEditNote> {
-
+    SharedTransitionLayout {
+        NavHost(navController = navController, startDestination = Screens.Notes) {
+            composable<Screens.Notes> {
+                val viewModel = hiltViewModel<NotesViewModel>()
+                val state = viewModel.state.value
+                NotesScreen(
+                    superInnerPadding = innerPadding,
+                    state = state,
+                    animatedVisibilityScope = this
+                ) {
+                    navController.navigate(Screens.AddEditNote)
+                }
+            }
+            composable<Screens.AddEditNote> {
+                AddEditNoteScreen(animatedVisibilityScope = this)
+            }
         }
     }
 }
