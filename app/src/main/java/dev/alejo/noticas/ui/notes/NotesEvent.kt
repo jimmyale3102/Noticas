914c078dcb3657bdce9c63e45b9366002ed5827c
Jimmyale3102 <jimmyale3102@gmail.com>
2025-04-17 15:52:05 +0700

    feat: Add GetAllNotes event to NotesEvent

--- app/src/main/java/dev/alejo/noticas/ui/notes/NotesEvent.kt
+++ app/src/main/java/dev/alejo/noticas/ui/notes/NotesEvent.kt
@@ -3,6 +3,7 @@ package dev.alejo.noticas.ui.notes
 import dev.alejo.noticas.domain.model.Note
 
 sealed class NotesEvent {
+    data object GetAllNotes : NotesEvent()
     data class DeleteNote(val note: Note) : NotesEvent()
     data class EditNote(val note: Note) : NotesEvent()
 }
