1e0d600d5796fb763332a312f884bb49c15e625f
Jimmyale3102 <jimmyale3102@gmail.com>
2025-04-17 15:48:56 +0700

    feat: Add ChangeTitle and ChangeContent events to AddEditNoteEvent

--- app/src/main/java/dev/alejo/noticas/ui/add_edit_note/AddEditNoteEvent.kt
+++ app/src/main/java/dev/alejo/noticas/ui/add_edit_note/AddEditNoteEvent.kt
@@ -2,5 +2,7 @@ package dev.alejo.noticas.ui.add_edit_note
 
 sealed class AddEditNoteEvent {
     data class ChangeColor(val color: Int) : AddEditNoteEvent()
+    data class ChangeTitle(val title: String) : AddEditNoteEvent()
+    data class ChangeContent(val content: String) : AddEditNoteEvent()
     data object SaveNote : AddEditNoteEvent()
 }
