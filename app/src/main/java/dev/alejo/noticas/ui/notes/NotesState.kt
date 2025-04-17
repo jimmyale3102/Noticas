02b6be87582ed3e366bcf7856198079d179e76d3
Jimmyale3102 <jimmyale3102@gmail.com>
2025-04-17 15:52:27 +0700

    refactor: Make NotesState.notes nullable

--- app/src/main/java/dev/alejo/noticas/ui/notes/NotesState.kt
+++ app/src/main/java/dev/alejo/noticas/ui/notes/NotesState.kt
@@ -3,7 +3,7 @@ package dev.alejo.noticas.ui.notes
 import dev.alejo.noticas.domain.model.Note
 
 data class NotesState(
-    val notes: List<Note> = emptyList()
+    val notes: List<Note>? = null
 )
 
 const val CREATE_NOTE_FAB_KEY = "CREATE_NOTE_FAB_KEY"
