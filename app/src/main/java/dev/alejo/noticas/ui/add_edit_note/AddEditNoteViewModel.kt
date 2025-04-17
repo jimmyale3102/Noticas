80358f8c1f44e84e5ec2ffff3ea53fa797ce6407
Jimmyale3102 <jimmyale3102@gmail.com>
2025-04-17 15:49:39 +0700

    feat: Add title and content fields to AddEditNoteState

--- app/src/main/java/dev/alejo/noticas/ui/add_edit_note/AddEditNoteViewModel.kt
+++ app/src/main/java/dev/alejo/noticas/ui/add_edit_note/AddEditNoteViewModel.kt
@@ -1,15 +1,23 @@
 package dev.alejo.noticas.ui.add_edit_note
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.alejo.noticas.domain.model.Note
+import dev.alejo.noticas.domain.usecase.AddNoteUseCase
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class AddEditNoteViewModel @Inject constructor() : ViewModel() {
+class AddEditNoteViewModel @Inject constructor(
+    private val addNoteUseCase: AddNoteUseCase
+) : ViewModel() {
 
     private val _state = MutableStateFlow(AddEditNoteState())
     val state : StateFlow<AddEditNoteState> = _state
@@ -22,7 +30,28 @@ class AddEditNoteViewModel @Inject constructor() : ViewModel() {
                 }
             }
             AddEditNoteEvent.SaveNote -> {
+                viewModelScope.launch(Dispatchers.IO) {
+                    addNoteUseCase(
+                        Note(
+                            title = state.value.title,
+                            content = state.value.content,
+                            color = state.value.backgroundColor.toArgb(),
+                            timestamp = System.currentTimeMillis()
+                        )
+                    )
+                }
+            }
 
+            is AddEditNoteEvent.ChangeTitle -> {
+                _state.update { state ->
+                    state.copy(title = event.title)
+                }
+            }
+
+            is AddEditNoteEvent.ChangeContent -> {
+                _state.update { state ->
+                    state.copy(content = event.content)
+                }
             }
         }
 
