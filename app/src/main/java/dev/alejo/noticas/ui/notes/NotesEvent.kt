7aea2f9e9121ab83e783059bbde7e2842756538c
Jimmyale3102 <jimmyale3102@gmail.com>
2025-04-18 14:29:05 +0700

    feat: Add search functionality to notes events

--- app/src/main/java/dev/alejo/noticas/ui/notes/NotesEvent.kt
+++ app/src/main/java/dev/alejo/noticas/ui/notes/NotesEvent.kt
@@ -5,4 +5,5 @@ import dev.alejo.noticas.domain.model.Note
 sealed class NotesEvent {
     data object GetAllNotes : NotesEvent()
     data class DeleteNote(val note: Note) : NotesEvent()
+    data class SearchNote(val text: String) : NotesEvent()
 }
