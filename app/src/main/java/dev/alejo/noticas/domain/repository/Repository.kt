9f647ff0708c4f458b5087710921c39421616b7a
Jimmyale3102 <jimmyale3102@gmail.com>
2025-04-18 14:30:13 +0700

    feat: Add getNotesBySearch method to Repository interface

--- app/src/main/java/dev/alejo/noticas/domain/repository/Repository.kt
+++ app/src/main/java/dev/alejo/noticas/domain/repository/Repository.kt
@@ -8,4 +8,5 @@ interface Repository {
     suspend fun getNoteById(id: Int): Note?
     fun getAllNotes(): Flow<List<Note>>
     suspend fun deleteNote(note: Note)
+    suspend fun getNotesBySearch(text: String): Flow<List<Note>>
 }
